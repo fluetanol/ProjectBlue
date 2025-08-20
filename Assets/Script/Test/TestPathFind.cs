86235ef3cdc2bd5ee269cc8ba6cfc2c5c029067a
fluetanol <etanolfly98@gmail.com>
2025-08-20 21:30:57 +0900

    feat: VFX Manager 추가
    말 그대로임

--- Assets/Script/Test/TestPathFind.cs
+++ Assets/Script/Test/TestPathFind.cs
@@ -7,37 +7,56 @@ using UnityEngine.AI;
 
 public class TestPathFind : MonoBehaviour
 {
-    public int RaycastCount = 10;
     public Rigidbody Rigidbody;
     public CapsuleCollider CapsuleCollider;
     public float Speed;
     public float SkinWidth = 0.2f;
     public Transform Destination;
+    
     public float PathfindTick = 0.1f;
 
-    private float _pathfindTick = 0;
     private bool isRight;
 
-    private NavMeshAgent agent;
+    public NavMeshAgent agent;
+
+    public NavMeshPath path;
+
+
 
     void Awake()
     {
         Rigidbody = GetComponent<Rigidbody>();
         CapsuleCollider = GetComponent<CapsuleCollider>();
         agent = GetComponent<NavMeshAgent>();
+        path = new NavMeshPath();
     }
 
     void Start()
     {
-        agent.SetDestination(Destination.position);
+        //agent.SetDestination(Destination.position);
+        print(agent.isOnNavMesh +" " + path.status);
+        if (agent.CalculatePath(Destination.position, path))
+        {
+            print("Path status: " + path.status);
+            agent.SetPath(path);
+
+            var corners = path.corners;
+            for (int i = 0; i < corners.Length - 1; i++)
+            {
+                Debug.DrawLine(corners[i], corners[i + 1], Color.green, 30f);
+            }
+
+        }
+       
     }
 
+    float _deltaTick = 0;
 
     void FixedUpdate()
     {
         //print(CapsuleCollider.bounds.center);
-        agent.SetDestination(Destination.position);
-        _pathfindTick = 0;
+        //agent.SetDestination(Destination.position);
+
         // pathfinding(CapsuleCollider.bounds.center);
 
         // Vector3 direction = (Destination.position - Rigidbody.position).normalized;
@@ -45,7 +64,14 @@ public class TestPathFind : MonoBehaviour
         // Rigidbody.MovePosition(Rigidbody.position + direction * Speed * Time.fixedDeltaTime);
 
         // _pathfindTick += Time.fixedDeltaTime;
-    
+
+        _deltaTick+=Time.fixedDeltaTime;
+        if (_deltaTick >= PathfindTick && agent.CalculatePath(Destination.position, path))
+        {
+            agent.SetPath(path);
+            _deltaTick = 0;
+            print("[NavMesh] Path status: " + path.status);
+        }
     }
 
 
