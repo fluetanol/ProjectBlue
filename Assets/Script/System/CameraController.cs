ab095c3a4a60f589cc48498282f89d03ff398266
fluetanol <etanolfly98@gmail.com>
2025-09-01 01:21:30 +0900

    feat: behavior test 중
    behavior tree 기능 테스트중

--- Assets/Script/System/CameraController.cs
+++ Assets/Script/System/CameraController.cs
@@ -1,6 +1,13 @@
 using System;
-using DG.Tweening;
 using UnityEngine;
+
+public enum CameraMode
+{
+    follow,
+    lookAt,
+    lookAtFollow,
+    free
+}
 
 [ExecuteAlways]
 public class CameraController : MonoBehaviour
@@ -8,6 +15,8 @@ public class CameraController : MonoBehaviour
     public static CameraController Instance;
 
     [SerializeField] private Transform _target;
+
+
     public Transform Target
     {
         get { return _target; }
